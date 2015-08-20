576ec1cf109ef208abb77be1c971346c9aad422e
Madalin Ignisca <madalin.ignisca@gmail.com>
2015-08-20 17:07:45 +0100

    editorconfig for the true g33k inside you

--- provision/first-time-init.sh
+++ provision/first-time-init.sh
@@ -4,10 +4,10 @@ export DEBIAN_FRONTEND=noninteractive
 if [ ! -f /root/.initial-provision ]
 then
 
-curl https://apt.puppetlabs.com/puppetlabs-release-wheezy.deb -o /tmp/puppetlabs-release-wheezy.deb
-dpkg -i /tmp/puppetlabs-release-wheezy.deb
+  curl https://apt.puppetlabs.com/puppetlabs-release-wheezy.deb -o /tmp/puppetlabs-release-wheezy.deb
+  dpkg -i /tmp/puppetlabs-release-wheezy.deb
 
-apt-get update
-apt-get install -y puppet
+  apt-get update
+  apt-get install -y puppet
 
 fi
